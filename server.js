18582f60a76ed4f6c3ac39cd1ea906aac6dc313a
zhukavets <stewart-8@mail.ru>
2022-11-19 00:28:21 +0300

    updates to deploy to heroku(update)

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 //Install express server
-const express = require('express');
-const path = require('path');
+let express = require('express');
+let path = require('path');
 
-const app = express();
+let app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/test-task-blog'));
@@ -14,4 +14,3 @@ app.get('/*', function(req,res) {
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
